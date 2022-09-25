c859362fb65182b6977704617dbd7506298193da
tahmid-23 <60953955+tahmid-23@users.noreply.github.com>
2022-09-25 08:02:31 -0400

    fix non-absolute paths

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use std::env::current_dir;
 use std::error::Error;
 use std::fmt::{Debug, Display, Formatter};
 use std::path::{Path, PathBuf};
@@ -10,13 +11,14 @@ use windows::core::HSTRING;
 use windows::Foundation::TypedEventHandler;
 use windows::Media::Core::MediaSource;
 use windows::Media::Playback::MediaPlayer;
-use windows::Media::SpeechSynthesis::{SpeechSynthesisStream, SpeechSynthesizer};
+use windows::Media::SpeechSynthesis::SpeechSynthesizer;
 use windows::Storage::{CreationCollisionOption, FileIO, StorageFile, StorageFolder};
 use windows::Storage::Streams::{DataReader, IBuffer};
 
 type Result<T> = std::result::Result<T, Box<dyn Error>>;
 
 #[derive(Parser)]
+#[clap(name = "WindowsTTS")]
 #[clap(version, about)]
 struct Args {
     /// Optional output file to write the audio to in .wav format instead of playing to audio device
@@ -59,8 +61,8 @@ async fn get_file(path: &Path) -> Result<StorageFile> {
 }
 
 async fn create_synth_text_buffer(message: &str) -> Result<IBuffer> {
-    let synth: SpeechSynthesizer = SpeechSynthesizer::new()?;
-    let synth_stream: SpeechSynthesisStream = synth.SynthesizeTextToStreamAsync(&HSTRING::from(message))?.await?;
+    let synth = SpeechSynthesizer::new()?;
+    let synth_stream = synth.SynthesizeTextToStreamAsync(&HSTRING::from(message))?.await?;
 
     let reader = DataReader::CreateDataReader(&synth_stream)?;
     let stream_size: u32 = synth_stream.Size()?.try_into()?;
@@ -82,21 +84,21 @@ async fn speak_to_file(message: &str, path: &Path) -> Result<()> {
 
 async fn speak_to_media_player(message: &str) -> Result<()> {
     let synth = SpeechSynthesizer::new()?;
-    let mp = MediaPlayer::new()?;
+    let media_player = MediaPlayer::new()?;
 
     let synth_stream = synth.SynthesizeTextToStreamAsync(&HSTRING::from(message))?.await?;
     let source = MediaSource::CreateFromStream(&synth_stream, &synth_stream.ContentType()?)?;
 
     let (tx, rx) = channel();
-    let end_token = mp.MediaEnded(&TypedEventHandler::new(move |_sender, _args| {
+    let end_token = media_player.MediaEnded(&TypedEventHandler::new(move |_sender, _args| {
         tx.send(()).unwrap();
         Ok(())
     }))?;
-    mp.SetSource(&source)?;
-    mp.Play()?;
+    media_player.SetSource(&source)?;
+    media_player.Play()?;
 
     rx.recv()?;
-    mp.RemoveMediaEnded(end_token)?;
+    media_player.RemoveMediaEnded(end_token)?;
 
     Ok(())
 }
@@ -105,7 +107,11 @@ fn main() -> Result<()> {
     let args = Args::parse();
     match args.output {
         Some(path) => {
-            block_on(speak_to_file(&args.message, &path))
+            let adjusted_path = match current_dir() {
+                Ok(current) => current.join(path),
+                Err(_) => path
+            };
+            block_on(speak_to_file(&args.message, &adjusted_path))
         }
         None => {
             block_on(speak_to_media_player(&args.message))
